b3458f15d1788cc9debc9e9701235d45fa7b3bb2
Vasylenko Orest <44865984+vasylenkorest@users.noreply.github.com>
2021-02-22 16:29:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const http = require("http");
-const url = require("url");
- 
-http.createServer(function(req, res){
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-    if(req.method == 'GET'){
-        let urlReq = url.parse(req.url, true);
-        console.log('urlReq : ', urlReq.query.key);
-        if(urlReq.query.key == '303'){
-            res.end('YESS');
-        } else {
-            res.end('NOOO');
-        }
-    }
-}).listen(3000);
+app.get('/', (req, res) => {
+  res.send('Test!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
